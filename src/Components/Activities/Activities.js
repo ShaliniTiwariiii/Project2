2127dfd49922dc7506836af38cc06854e238c7ae
ShaliniTiwariiii <salinitiwari12345@gmail.com>
2023-01-15 19:12:17 +0530

    final submission

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,39 +1,4 @@
-// import React from "react";
-// // import { useRecoilValue } from "recoil";
-// // import { loginAtom } from "../../atom/Atom";
-// // import { atom, useRecoilState, useRecoilValue } from "recoil";
-// // import { loginAtom } from "../../atom/Atom";
-// // import { selectorValue } from "../../selector/selector";
-// import "./Activities.css";
-// // import { useRecoilState } from "recoil";
-// // import { loginAtom } from "../../atom/Atom";
 
-// function Activities(atomData) {
-//   // const [logValue, setLogValue] = useRecoilState(loginAtom);
-//   // const planData =useRecoilValue(loginAtom)
-//   let cards = [
-//     "activity1 wake up ea",
-//     "activity1",
-//     "activity1",
-//     "activity1",
-//     "activity1",
-//     "activity1",
-//   ];
-//   // const planData = useRecoilValue(selectorValue);
-//   // console.log(planData)
-  
-//   return (
-//     <>
-//       <div className="activity">
-//         {cards.map((value) => (
-//           <div className="activity-c">{value}</div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Activities;
 
 
 
@@ -68,7 +33,7 @@ function Activities(atomData) {
   // const planData = useRecoilValue(selectorValue);
   // console.log(planData)
   
-  
+
   return (
     <>
       <div className="activity">
